f43811515d80bbca7adb09e8baaaa2bee186efc7
ammwangi <elijah.mwangi@kamsware.com>
2025-09-09 12:31:12 +0300

    feat:  oauth mnodels

---
+++ Ecommerce.Application/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password), StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password), Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string CellPhone { get; set; }
+    }
+}
