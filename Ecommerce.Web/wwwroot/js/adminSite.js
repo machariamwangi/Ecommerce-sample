2f5fa2122fa860ac97cf0234e3a121888865799d
ammwangi <elijah.mwangi@kamsware.com>
2025-09-10 10:07:45 +0300

    feat: admin ui complete

---
+++ Ecommerce.Web/wwwroot/js/adminSite.js
@@ -0,0 +1,7 @@
+﻿var toggle = document.getElementById('menu-toggle');
+if (toggle) {
+    toggle.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.getElementById('sidebar').classList.toggle('d-none');
+    });
+}
