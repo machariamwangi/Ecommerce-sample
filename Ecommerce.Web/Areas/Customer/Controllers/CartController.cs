84b45f106e814da838a62f627212793342cf9ef2
ammwangi <elijah.mwangi@kamsware.com>
2025-09-10 10:06:57 +0300

    feat: customer view and controoler

--- Ecommerce.Web/Areas/Customer/Controllers/CartController.cs
+++ Ecommerce.Web/Areas/Customer/Controllers/CartController.cs
@@ -1,19 +1,64 @@
 ﻿using Ecommerce.Application.Services.CustomerServices;
+using Ecommerce.Application.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.Web.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class CartController : Controller
     {
-       private readonly CartService _cartService;
+        private readonly CartService _cartSerivce;
+        public CartVM CartVM { get; set; }
 
-        public CartController(CartService cartService)
+        public CartController(CartService cartSerivce)
         {
-            _cartService = cartService;
+            _cartSerivce = cartSerivce;
+
         }
+
         public IActionResult Index()
         {
-            return View();
+            var cartVM = _cartSerivce.GetCartForUserAsync(User);
+            if (cartVM == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            return View(cartVM);
+        }
+
+        public IActionResult Order()
+        {
+            var cartVM = _cartSerivce.GetCartViewModelAsync();
+            if (cartVM == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            return View(cartVM);
+        }
+
+        [HttpPost]
+        [ActionName("Order")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> OrderPost(CartVM cartVM)
+        {
+            bool success = _cartSerivce.PlaceOrderAsync(cartVM);
+            if (!success)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            return RedirectToAction(nameof(Index), "Home", new { area = "Customer" });
+        }
+
+        public IActionResult Increase(int cartId)
+        {
+            bool success = _cartSerivce.IncreaseCartItem(cartId);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Decrease(int cartId)
+        {
+            _cartSerivce.DecreaseCartItem(cartId);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Ecommerce.Web/Areas/Customer/Controllers/OrderController.cs
+++ Ecommerce.Web/Areas/Customer/Controllers/OrderController.cs
@@ -1,21 +1,34 @@
 ﻿using Ecommerce.Application.Services.AdminServices;
+using Ecommerce.Application.Services.CustomerServices;
+using Ecommerce.Application.ViewModels;
 using Ecommerce.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.Web.Areas.Customer.Controllers
 {
+    [Area("Customer")]
+    [Authorize]
     public class OrderController : Controller
     {
-        private OrderService _orderService;
+        private readonly CustomerOrderService _orderService;
+        public OrderVM OrderVM { get; set; }
 
-        public OrderController(OrderService orderService)
+        public OrderController(CustomerOrderService orderService)
         {
             _orderService = orderService;
         }
+
         public IActionResult Index()
         {
-            //IEnumerable<Product> productList = _orderService.Get();
-            return View();
+            IEnumerable<OrderProduct> orderProducts = _orderService.GetUserOrdersAsync();
+            return View(orderProducts);
+        }
+
+        public IActionResult CancelOrder(int id)
+        {
+            _orderService.CancelOrder(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
